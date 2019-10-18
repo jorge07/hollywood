3ee4f9ad659c824106a2d0d585ef8ee1a90456d1
Jorge Arco <jorge.arcoma@gmail.com>
2019-10-19 01:21:14 +0200

    autowiring support for innversify

---
+++ src/Application/Bus/autoMap.ts
@@ -0,0 +1,11 @@
+function autowiring<T>(target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<T>) {
+    const value: any = Reflect.getMetadata(
+        "design:paramtypes",
+        target,
+        "handle",
+    );
+
+    target.command = {
+        name: value[0].name,
+    };
+}
